f8cfa85da42e8e9c3d8e9bf2d9efbe541da0814e
Coac <levictor3000@hotmail.fr>
2017-03-05 16:20:17 +0100

    Initial commit

---
+++ HierarchyInGame.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HierarchyInGame : MonoBehaviour {
+
+    private List<GameObject> rootObjects = new List<GameObject>();
+	void Start () {
+        this.updateRootObjects();
+    }
+
+    private void updateRootObjects()
+    {
+        this.rootObjects.Clear();
+        foreach (Transform xform in UnityEngine.Object.FindObjectsOfType<Transform>())
+        {
+            if (xform.parent == null)
+            {
+                rootObjects.Add(xform.gameObject);
+            }
+        }
+    }
+
+
+    void OnGUI()
+    {
+        hierachyRect = GUI.Window(0, hierachyRect, HierarchyFunction, "Hierarchy");
+        Inspector.draw(new Rect(Screen.width - 400, 0, 400, Screen.height));
+    }
+
+    private Rect hierachyRect = new Rect(0, 0, 400, Screen.height);
+    private Vector2 scrollViewVector = Vector2.zero;
+    void HierarchyFunction(int windowID)
+    {
+        if (GUILayout.Button("Update GameObjects"))
+        {
+            this.updateRootObjects();
+        }
+
+        int contentHeight = Screen.height * 2;
+        int scrollViewMargin = 20;
+        int contentWidth = (int)hierachyRect.width - scrollViewMargin - 20;
+
+        scrollViewVector = GUI.BeginScrollView(new Rect(scrollViewMargin, 50, hierachyRect.width - scrollViewMargin - 10, hierachyRect.height), scrollViewVector, new Rect(0, 0, contentWidth, contentHeight));
+    
+            GUILayout.BeginArea(new Rect(0, 0, contentWidth, contentHeight));
+
+                foreach (GameObject go in this.rootObjects)
+                {
+                    gameObjectDisplay(go);
+                }
+
+            GUILayout.EndArea();
+
+        GUI.EndScrollView();
+    }
+
+
+
+    void gameObjectDisplay(GameObject go)
+    {
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button(go.transform.name))
+        {
+            Debug.Log("lol");
+        }
+
+        if (GUILayout.Button("->", GUILayout.Width(30)))
+        {
+            Inspector.displayComponents(go);
+        }
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ Inspector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+public class Inspector : MonoBehaviour
+{
+
+    public static void draw(Rect screenRect)
+    {
+        Inspector.inspectorRect = screenRect;
+        inspectorRect = GUI.Window(1, inspectorRect, inspectorFunction, "Inspector");
+    }
+
+    static public string text = "";
+    static private Rect inspectorRect = new Rect(Screen.width - 400, 0, 400, Screen.height);
+    static private Vector2 scrollViewVector = Vector2.zero;
+    static void inspectorFunction(int windowID)
+    {
+
+        scrollViewVector = GUI.BeginScrollView(new Rect(0, 20, Inspector.inspectorRect.width, Inspector.inspectorRect.height), scrollViewVector, new Rect(0, 0, Inspector.inspectorRect.width, Inspector.inspectorRect.height*10));
+        GUILayout.TextArea(Inspector.text);
+        GUI.EndScrollView();
+
+    }
+
+    public static void displayComponents(GameObject go)
+    {
+        Inspector.text = "";
+        foreach (Component co in go.GetComponents(typeof(Component)))
+        {
+            Type t = co.GetType();
+            Inspector.text += "\nType " + t;
+            Inspector.text += "\nType information for:" + t.FullName;
+            Inspector.text += "\n\tBase class = " + t.BaseType.FullName;
+            Inspector.text += "\n\tIs Class = " + t.IsClass;
+            Inspector.text += "\n\tIs Enum = " + t.IsEnum;
+            Inspector.text += "\n\tAttributes = " + t.Attributes;
+            System.Reflection.FieldInfo[] fieldInfo = t.GetFields();
+            foreach (System.Reflection.FieldInfo info in fieldInfo)
+                Inspector.text += "\nField:" + info.Name;
+            System.Reflection.PropertyInfo[] propertyInfo = t.GetProperties();
+            foreach (System.Reflection.PropertyInfo info in propertyInfo)
+                Inspector.text += "\nProp:" + info.Name;
+        }
+        
+    }
+}
