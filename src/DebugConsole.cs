ccd71c5a70033cdb389afb2a391e34fc13309963
Coac <levictor3000@hotmail.fr>
2017-03-09 17:10:02 +0100

    Log color based on logtype

--- src/DebugConsole.cs
+++ src/DebugConsole.cs
@@ -16,12 +16,23 @@ public class DebugConsole
 {
 
     private Rect debugConsoleRect;
-    private string logs;
+    private List<Log> logs;
+
+    private class Log {
+        public string log;
+        public LogType logType;
+
+        public Log(string log, LogType logType)
+        {
+            this.log = log;
+            this.logType = logType;
+        }
+    }
 
     public DebugConsole(Rect debugConsoleRect)
     {
         this.debugConsoleRect = debugConsoleRect;
-        this.logs = "";
+        this.logs = new List<Log>();
         Application.logMessageReceived += this.logHandler;
     }
 
@@ -34,15 +45,33 @@ public class DebugConsole
 
     void logHandler(string message, string stackTrace, LogType type)
     {
-        logs += message + "\n";
+        this.logs.Insert(0, new Log(message + stackTrace, type));
     }
 
     private void debugConsoleFunction(int windowID)
     {
         this.scrollViewVector = GUILayout.BeginScrollView(this.scrollViewVector);
 
-        GUILayout.TextArea(this.logs);
-    
+        foreach(Log log in logs)
+        {
+            GUIStyle style = new GUIStyle();
+
+            switch(log.logType)
+            {
+                case LogType.Error:
+                    style.normal.textColor = Color.red;
+                    break;
+                case LogType.Warning:
+                    style.normal.textColor = Color.yellow;
+                    break;
+                default:
+                    style.normal.textColor = Color.white;
+                    break;
+            }
+
+            GUILayout.TextArea(log.log, style);
+        }
+
         GUILayout.EndScrollView();
     }
 
